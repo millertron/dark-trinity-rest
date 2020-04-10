0c5e65fbfec300043371f954f40ab8cec4fde7b2
Koha Choji <koha.choji@vistair.com>
2020-04-10 15:10:07 +0100

    project setup

---
+++ src/main/java/com/millertronics/darktrinityrest/DarkTrinityRestApplication.java
@@ -0,0 +1,13 @@
+package com.millertronics.darktrinityrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkTrinityRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkTrinityRestApplication.class, args);
+	}
+
+}
